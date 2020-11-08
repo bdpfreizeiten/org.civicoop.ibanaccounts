c80d9911fc9b1a9a0dd4410f7e133315a8c0a1ba
bdpfreizeiten <dave1111@gmx.net>
2020-11-08 19:53:27 +0100

    Update Ibanaccounts.php

--- CRM/Ibanaccounts/Ibanaccounts.php
+++ CRM/Ibanaccounts/Ibanaccounts.php
@@ -1,313 +1,257 @@
 <?php
 
-require_once 'ibanaccounts.civix.php';
-require_once('php-iban/oophp-iban.php');
+require_once(__DIR__.'./../../php-iban/oophp-iban.php');
 
-/**
- * Check if an iban is in use by a membership
+/* 
+ * Retrieve and save IBAN accounts
  * 
- * @param string $iban
- * @param int|bool $contactId
- * @return array
  */
-function ibanaccounts_civicrm_iban_usages($iban, $contactId = false) {
-  $config = CRM_Ibanaccounts_Config::singleton();
-  $table = $config->getIbanMembershipCustomGroupValue('table_name');
-  $iban_field = $config->getIbanMembershipCustomFieldValue('column_name');
+
+class CRM_Ibanaccounts_Ibanaccounts {
   
-  $sql = "SELECT `m`.`id` AS `id`, `mtype`.`name` FROM `".$table."` `i` INNER JOIN `civicrm_membership` `m` ON `i`.`entity_id`  = `m`.`id` INNER JOIN `civicrm_membership_type` `mtype` ON `m`.`membership_type_id`  = `mtype`.`id` WHERE `i`.`".$iban_field."` = %1 AND `m`.`contact_id` = %2";
-  $dao = CRM_Core_DAO::executeQuery($sql, array(
-    '1' => array($iban, 'String'),
-    '2' => array($contactId, 'Integer'),
-  ));
-  $return = array();
-  while($dao->fetch()) {
-    $return['civicrm_membership'][$dao->id] = ts("IBAN Account is used for membership %1", array(1 => $dao->name));
-  }
-  
-  //check if iban is in use at contribution level
-  $table = $config->getIbanContributionCustomGroupValue('table_name');
-  $iban_field = $config->getIbanContributionCustomFieldValue('column_name');
-  $sql = "SELECT COUNT(`m`.`id`) AS `contribution_count` FROM `".$table."` `i` INNER JOIN `civicrm_contribution` `m` ON `i`.`entity_id`  = `m`.`id` WHERE `i`.`".$iban_field."` = %1 AND `m`.`contact_id` = %2";
-  $dao = CRM_Core_DAO::executeQuery($sql, array(
-    '1' => array($iban, 'String'),
-    '2' => array($contactId, 'Integer'),
-  ));
-  $return = array();
-  while($dao->fetch() && $dao->contribution_count) {
-    $return['civicrm_contribution'][1] = ts("IBAN Account is used for %1 contributions", array(1 => $dao->contribution_count));
-  }
-  return $return;
-} 
-
-
-
-/**
- * Implementatio of hook__civicrm_tabs
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_tabs
- */
-function ibanaccounts_civicrm_tabs(&$tabs, $contactID) {
-
-  $config = CRM_Ibanaccounts_Config::singleton();
-  
-  //unset the tab for iban accounts via custom fields and set our own tab for 
-  //display the iban accounts
-  $tab_id = 'custom_'.$config->getIbanCustomGroupValue('id'); 
-  foreach($tabs as $key => $tab) {
-   if ($tab['id'] == $tab_id) {
-     unset($tabs[$key]);
-   } 
-  }
-
-  if (CRM_Core_Permission::check('access CiviContribute')) {
-    $url = CRM_Utils_System::url('civicrm/contact/ibanaccount/view', "cid=$contactID&snippet=1");
-
-    //Count rules
-    $accounts = CRM_Ibanaccounts_Ibanaccounts::IBANForContact($contactID);
-    $tabs[] = array(
-      'id' => 'iban_accounts',
-      'url' => $url,
-      'count' => count($accounts),
-      'title' => ts('IBAN Accounts'),
-      'weight' => -100
-    );
-  }
-}
-
-/**
- * Validate the entered IBAN account number
- * 
- * 
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_validateForm
- * @param type $formName
- * @param type $fields
- * @param type $files
- * @param type $form
- * @param type $errors
- */
-function ibanaccounts_civicrm_validateForm( $formName, &$fields, &$files, &$form, &$errors ) {
-  if ($formName == 'CRM_Contact_Form_CustomData') {    
+  /*
+   * Returns an array with all IBAN numbers belonging to a contact
+   * 
+   * Return array consist of the following details per IBAN
+   * id => id of the IBAN
+   * contact_id => contact ID
+   * iban => the IBAN number
+   * bic => the BIC number
+   */
+  public static function IBANForContact($contactId) {        
+    if (empty($contactId)) {
+      return array();
+    }
+    
     $config = CRM_Ibanaccounts_Config::singleton();
-    $groupId = $form->getVar('_groupID');
-    if ($groupId != $config->getIbanCustomGroupValue('id')) {
+    $table = $config->getIbanCustomGroupValue('table_name');
+    $iban_field = $config->getIbanCustomFieldValue('column_name');
+    $bic_field = $config->getBicCustomFieldValue('column_name');
+    $tnv_field = $config->getTnvCustomFieldValue('column_name');
+    $sql = "SELECT * FROM `".$table."`  WHERE `entity_id`  = %1";
+    $dao = CRM_Core_DAO::executeQuery($sql, array(
+      '1' => array($contactId, 'Integer'),
+    ));
+    
+    $return = array();
+    while($dao->fetch()) {
+      $iban = new IBAN($dao->$iban_field);
+      
+      $account['id'] = $dao->id;
+      $account['contact_id'] = $dao->entity_id;
+      $account['iban'] = $iban->MachineFormat();
+      $account['iban_human'] = $iban->HumanFormat();
+      $account['bic'] = $dao->$bic_field;
+      $account['tnv'] = $dao->$tnv_field;
+      $return[$dao->id] = $account;
+    }
+    return $return;
+  }
+  
+  /*
+   * Returns an array with all IBAN number
+   * 
+   * Return array consist of the following details per IBAN
+   * id => id of the IBAN
+   * contact_id => contact ID
+   * iban => the IBAN number
+   * bic => the BIC number
+   */
+  public static function findIBANByIban($iban) {
+    $config = CRM_Ibanaccounts_Config::singleton();
+    $table = $config->getIbanCustomGroupValue('table_name');
+    $iban_field = $config->getIbanCustomFieldValue('column_name');
+    $bic_field = $config->getBicCustomFieldValue('column_name');
+    $tnv_field = $config->getTnvCustomFieldValue('column_name');
+    $sql = "SELECT * FROM `".$table."`  WHERE `".$iban_field."`  = %1";
+    $dao = CRM_Core_DAO::executeQuery($sql, array(
+      '1' => array($iban, 'String'),
+    ));
+    
+    $return = array();
+    while($dao->fetch()) {
+      $account['id'] = $dao->id;
+      $account['contact_id'] = $dao->entity_id;
+      $account['iban'] = $dao->$iban_field;
+      $account['bic'] = $dao->$bic_field;
+      $account['tnv'] = $dao->$tnv_field;
+      $return[$dao->id] = $account;
+    }
+    return $return;
+  }
+  
+  /**
+   * Saves an IBAN Number for a contact
+   * 
+   * @param string $iban
+   * @param string $bic
+   * @param string $tnv
+   * @param int $contactId
+   * @return int the id of the IBAN
+   */
+  public static function saveIBANForContact($iban, $bic, $tnv, $contactId) {
+    if (empty($iban)) {
       return;
     }
+    
+    $iban_class = new IBAN($iban);
+    $iban_system = $iban_class->MachineFormat();
 
-    $iban = new IBAN();
-    foreach($fields as $key => $value) {
-      if (strpos($key, "custom_".$config->getIbanCustomFieldValue('id'))===0) {
-        if (!$iban->Verify($value)) {
-          $errors[$key] = ts("'".$value."' is not a valid IBAN");
+    //only save when IBAN number doesn't exist yet
+
+    $config = CRM_Ibanaccounts_Config::singleton();
+    $iban_field = 'custom_'.$config->getIbanCustomFieldValue('id');
+    $bic_field = 'custom_'.$config->getBicCustomFieldValue('id');
+    $tnv_field = 'custom_'.$config->getTnvCustomFieldValue('id');
+
+    $id = self::getIdByIBANAndContactId($iban_system, $contactId);
+    if ($id) {
+      //iban number already exist
+      $iban_field = 'custom_'.$config->getIbanCustomFieldValue('id').'_'.$id;
+      $bic_field = 'custom_'.$config->getBicCustomFieldValue('id').'_'.$id;
+      $tnv_field = 'custom_'.$config->getTnvCustomFieldValue('id').'_'.$id;
+    }
+
+    $params = array();
+    $params['entityID'] = $contactId;
+    $params[$iban_field] = $iban_system;
+    $params[$bic_field] = $bic;
+    $params[$tnv_field] = $tnv;
+    if (empty($params[$bic_field])) {
+      $params[$bic_field] = CRM_Ibanaccounts_Utils_IbanToBic::getBic($params[$iban_field]);
+    }
+
+    CRM_Core_BAO_CustomValueTable::setValues($params);
+    //civicrm_api3('CustomValue', 'Create', $params);
+
+    return self::getIdByIBANAndContactId($iban, $contactId);
+  }
+
+  /**
+   * Get the ID of an IBAN Number
+   *
+   * @param type $iban
+   * @param int|bool $contactId
+   * @return false|int
+   */
+  public static function getIdByIBANAndContactId($iban, $contactId = false) {
+    $config = CRM_Ibanaccounts_Config::singleton();
+    $table = $config->getIbanCustomGroupValue('table_name');
+    $iban_field = $config->getIbanCustomFieldValue('column_name');
+    
+    $iban_class = new IBAN($iban);
+    $iban_system = $iban_class->MachineFormat();
+    
+    $sql = "SELECT * FROM `".$table."` WHERE `".$iban_field."` = %1";
+    $params[1] = array($iban_system, 'String');
+    if ($contactId) {
+      $sql .= " AND `entity_id` = %2";
+      $params[2] = array($contactId, 'Integer');
+    }
+    $dao = CRM_Core_DAO::executeQuery($sql, $params);
+    if ($dao->fetch()) {
+      return $dao->id;
+    }
+    return false;
+  }
+  
+  /**
+   * Returns an array with usages for the IBAN account.
+   * 
+   * The array contains description for uses of the IBAN account e.g.:
+   * array(
+   *  'IBAN account in use by membership',
+   * )
+   * 
+   * If the iban account is not in use by any entity then the array should be empty and the IBAN could be deleted safely
+   * 
+   * @param type $iban
+   * @return array
+   */
+  public static function getIBANUsages($iban, $contactId = false) {
+    
+    $iban_class = new IBAN($iban);
+    $iban_system = $iban_class->MachineFormat();
+    
+    $hooks = CRM_Ibanaccounts_Utils_HookInvoker::singleton();
+    $usages = $hooks->hook_civicrm_iban_usages($iban_system, $contactId);
+    if (!is_array($usages)) {
+      $usages = array();
+    }
+    $messages = array();
+    foreach($usages as $entities) {
+      foreach($entities as $entity) {
+        $messages[] = $entity;
+      }
+    }
+    return $messages;
+  }
+  
+  public static function removeIban($iban, $contactId) {
+    $iban_class = new IBAN($iban);
+    $iban_system = $iban_class->MachineFormat();
+    
+    $hooks = CRM_Ibanaccounts_Utils_HookInvoker::singleton();
+    $hooks->hook_civicrm_remove_iban($iban_system, $contactId);
+    
+    $config = CRM_Ibanaccounts_Config::singleton();
+    $iban_field_id = $config->getIbanCustomFieldValue('id');
+    
+    //fetch id
+    $contactParams = array();
+    $contactParams['id'] = $contactId;
+    $contactParams['custom_'.$iban_field_id] = $iban;
+    $contactParams['return.custom_'.$iban_field_id] = 1;
+    $contact = civicrm_api3('Contact', 'getsingle', $contactParams);
+    //extract the custom value table id
+    $objectId = $contact[$config->getIbanCustomGroupValue('table_name').'_id'];
+    
+    CRM_Core_BAO_CustomValue::deleteCustomValue($objectId, $config->getIbanCustomGroupValue('id'));
+  }
+  
+  public static function saveIbanForMembership($mid, $contactId, $iban, $bic, $tnv, $iban_account_id = false) {
+    $config = CRM_Ibanaccounts_Config::singleton();
+    $table = $config->getIbanMembershipCustomGroupValue('table_name');
+    $iban_field = $config->getIbanMembershipCustomFieldValue('column_name');
+    $bic_field = $config->getBicMembershipCustomFieldValue('column_name');
+    $tnv_field = $config->getTnvMembershipCustomFieldValue('column_name');
+    
+    //remove the current bank account
+    CRM_Core_DAO::executeQuery("DELETE FROM `" . $table . "` WHERE `entity_id` = %1", array(1 => array($mid, 'Integer')));
+
+    $accounts = CRM_Ibanaccounts_Ibanaccounts::IBANForContact($contactId);
+
+    if ($iban_account_id == -1 || !isset($accounts[$iban_account_id])) {
+      $iban_account_id = CRM_Ibanaccounts_Ibanaccounts::saveIBANForContact($iban, $bic, $tnv, $contactId);
+      $accounts = CRM_Ibanaccounts_Ibanaccounts::IBANForContact($contactId);
+    }
+
+    if (isset($accounts[$iban_account_id])) {
+      $_iban = $accounts[$iban_account_id]['iban'];
+      $_bic = $accounts[$iban_account_id]['bic'];
+      $_tnv = $accounts[$iban_account_id]['tnv'];
+
+      $sql = "INSERT INTO `" . $table . "` (`entity_id`, `" . $iban_field . "`, `" . $bic_field . "`, `".$tnv_field."`) VALUES (%1, %2, %3, %4);";
+      CRM_Core_DAO::executeQuery($sql, array(
+            '1' => array($mid, 'Integer'),
+            '2' => array($_iban, 'String'),
+            '3' => array($_bic, 'String'),
+            '4' => array($_tnv, 'String'),
+      ));
+      
+      //also record iban information on the contribution record
+      $membership_payment = new CRM_Member_BAO_MembershipPayment();
+      $membership_payment->membership_id = $mid;
+      $membership_payment->find(false);
+      while($membership_payment->fetch()) {
+        $postMembershipPayment = new CRM_Ibanaccounts_Post_MembershipPayment();
+        $isFutureContribution = CRM_Core_DAO::singleValueQuery("SELECT COUNT(*) FROM `civicrm_contribution` WHERE `id` = %1", array(1=>array($membership_payment->contribution_id, 'Integer')));
+        if ($isFutureContribution) {
+          $postMembershipPayment->clearIban($membership_payment->contribution_id);
+          $postMembershipPayment->saveIban($membership_payment->contribution_id, $_iban, $_bic, $_tnv);
         }
       }
     }
-  } elseif ($formName == 'CRM_Member_Form_Membership') {
-    $config = CRM_Ibanaccounts_Config::singleton();
-    if ($config->isIbanMembershipEnabled()) {
-      $membership = new CRM_Ibanaccounts_Buildform_Membership($form);
-      $membership->validateForm($fields, $files, $errors);
-    }
   }
-
-  if ($formName == 'CRM_Contribute_Form_Contribution') {
-    $config = CRM_Ibanaccounts_Config::singleton();
-    if ($config->isIbanContributionshipEnabled()) {
-     $contribution = new CRM_Ibanaccounts_Buildform_Contribution($form);
-     $contribution->validateForm($fields, $files, $errors);
-    }
-  }
-
-  if ($formName == 'CRM_Member_Form_MembershipRenewal') {
-    $config = CRM_Ibanaccounts_Config::singleton();
-    if ($config->isIbanMembershipEnabled()) {
-      $membership = new CRM_Ibanaccounts_Buildform_MembershipRenewal($form);
-      $membership->validateForm($fields, $files, $errors);
-    }
- }
-
-}
-
-/**
- * 
- * Implementation of hook_civicrm_buildForm
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_buildForm
- */
-function ibanaccounts_civicrm_buildForm($formName, &$form) {
- if ($formName == 'CRM_Member_Form_Membership') {
-   //add template 
-  $config = CRM_Ibanaccounts_Config::singleton();
-  if ($config->isIbanMembershipEnabled()) {
-    $membership = new CRM_Ibanaccounts_Buildform_Membership($form);
-    $membership->parse();
-  }
- } 
- if ($formName == 'CRM_Member_Form_MembershipRenewal') {
-   $config = CRM_Ibanaccounts_Config::singleton();
-   if ($config->isIbanMembershipEnabled()) {
-    $membership = new CRM_Ibanaccounts_Buildform_MembershipRenewal($form);
-    $membership->parse();
-   }
- }
- if ($formName == 'CRM_Contribute_Form_Contribution') {
-   $config = CRM_Ibanaccounts_Config::singleton();
-   if ($config->isIbanContributionshipEnabled()) {
-    $contribution = new CRM_Ibanaccounts_Buildform_Contribution($form);
-    $contribution->parse();
-   }
- }
-}
-
-/**
- * 
- * Implementation of hook_civicrm_postProcess
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postProcess
- */
-function ibanaccounts_civicrm_postProcess( $formName, &$form ) {
-  if ($formName == 'CRM_Member_Form_Membership') {
-    $config = CRM_Ibanaccounts_Config::singleton();
-    if ($config->isIbanMembershipEnabled()) {
-     $membership = new CRM_Ibanaccounts_Buildform_Membership($form);
-     $membership->postProcess();
-    }
-  }
-  if ($formName == 'CRM_Member_Form_MembershipRenewal') {
-    $config = CRM_Ibanaccounts_Config::singleton();
-    if ($config->isIbanMembershipEnabled()) {
-      $membership = new CRM_Ibanaccounts_Buildform_MembershipRenewal($form);
-      $membership->postProcess();
-    }
- }
-  if ($formName == 'CRM_Contribute_Form_Contribution') {
-    $config = CRM_Ibanaccounts_Config::singleton();
-    if ($config->isIbanContributionshipEnabled()) {
-      $contribution = new CRM_Ibanaccounts_Buildform_Contribution($form);
-      $contribution->postProcess();
-    }
- }
-}
-
-/**
- * Implementation of hook_civicrm_post
- * 
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_post
- * @param type $op
- * @param type $objectName
- * @param type $objectId
- * @param type $objectRef
- */
-function ibanaccounts_civicrm_post( $op, $objectName, $objectId, &$objectRef ) {
-  if ($objectName == 'MembershipPayment' ) {
-    $config = CRM_Ibanaccounts_Config::singleton();
-    if ($config->isIbanMembershipEnabled()) {
-      $membership_payment = new CRM_Ibanaccounts_Post_MembershipPayment();
-      $membership_payment->post($op, $objectRef);
-    }
-  }
-}
-
-/**
- * Implementation of hook_civicrm_config
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_config
- */
-function ibanaccounts_civicrm_config(&$config) {
-  _ibanaccounts_civix_civicrm_config($config);
-}
-
-/**
- * Implementation of hook_civicrm_xmlMenu
- *
- * @param $files array(string)
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function ibanaccounts_civicrm_xmlMenu(&$files) {
-  _ibanaccounts_civix_civicrm_xmlMenu($files);
-}
-
-/**
- * Implementation of hook_civicrm_install
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
- */
-function ibanaccounts_civicrm_install() {
-  return _ibanaccounts_civix_civicrm_install();
-}
-
-/**
- * Implementation of hook_civicrm_uninstall
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function ibanaccounts_civicrm_uninstall() {
-  return _ibanaccounts_civix_civicrm_uninstall();
-}
-
-/**
- * Implementation of hook_civicrm_enable
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
- */
-function ibanaccounts_civicrm_enable() {
-  return _ibanaccounts_civix_civicrm_enable();
-}
-
-/**
- * Implementation of hook_civicrm_disable
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function ibanaccounts_civicrm_disable() {
-  return _ibanaccounts_civix_civicrm_disable();
-}
-
-/**
- * Implementation of hook_civicrm_upgrade
- *
- * @param $op string, the type of operation being performed; 'check' or 'enqueue'
- * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
- *
- * @return mixed  based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
- *                for 'enqueue', returns void
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function ibanaccounts_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _ibanaccounts_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implementation of hook_civicrm_managed
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function ibanaccounts_civicrm_managed(&$entities) {
-  return _ibanaccounts_civix_civicrm_managed($entities);
-}
-
-/**
- * Implementation of hook_civicrm_caseTypes
- *
- * Generate a list of case-types
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function ibanaccounts_civicrm_caseTypes(&$caseTypes) {
-  _ibanaccounts_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implementation of hook_civicrm_alterSettingsFolders
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function ibanaccounts_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _ibanaccounts_civix_civicrm_alterSettingsFolders($metaDataFolders);
+  
 }
